4ea3b05cd94182f8e6b57a9b597b7aaeee3cd080
ElXreno <elxreno@gmail.com>
2019-08-07 10:39:27 +0300

    LocationModule: Use context instead activity

--- app/src/main/java/com/elxreno/weather/di/modules/LocationModule.kt
+++ app/src/main/java/com/elxreno/weather/di/modules/LocationModule.kt
@@ -1,6 +1,5 @@
 package com.elxreno.weather.di.modules
 
-import android.app.Activity
 import android.content.Context
 import android.location.LocationManager
 import dagger.Module
@@ -12,6 +11,6 @@ class LocationModule {
 
     @Provides
     @Singleton
-    fun provideLocationService(activity: Activity) =
-        activity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    fun provideLocationService(context: Context) =
+        context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 }
