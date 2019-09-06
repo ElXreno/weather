a01a9e5a7f396b0a8bf4552598d08fb1fca2285c
ElXreno <elxreno@gmail.com>
2019-09-06 20:02:22 +0300

    Use device language for receiving data

--- app/src/main/java/com/elxreno/weather/data/api/WeatherApi.kt
+++ app/src/main/java/com/elxreno/weather/data/api/WeatherApi.kt
@@ -5,32 +5,37 @@ import com.elxreno.weather.data.dto.ForecastWeatherDto
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.*
 
 interface WeatherApi {
 
     @GET("weather")
     fun getWeatherTodayById(
         @Query("id") id: Int,
-        @Query("units") units: String = "metric"
+        @Query("units") units: String = "metric",
+        @Query("lang") lang: String = Locale.getDefault().language
     ): Observable<CurrentWeatherDto>
 
     @GET("weather")
     fun getWeatherTodayByCoordinates(
         @Query("lat") lat: Double,
         @Query("lon") lon: Double,
-        @Query("units") units: String = "metric"
+        @Query("units") units: String = "metric",
+        @Query("lang") lang: String = Locale.getDefault().language
     ): Observable<CurrentWeatherDto>
 
     @GET("forecast")
     fun getWeatherForecastById(
         @Query("id") id: Int,
-        @Query("units") units: String = "metric"
+        @Query("units") units: String = "metric",
+        @Query("lang") lang: String = Locale.getDefault().language
     ): Observable<ForecastWeatherDto>
 
     @GET("forecast")
     fun getWeatherForecastByCoordinates(
         @Query("lat") lat: Double,
         @Query("lon") lon: Double,
-        @Query("units") units: String = "metric"
+        @Query("units") units: String = "metric",
+        @Query("lang") lang: String = Locale.getDefault().language
     ): Observable<ForecastWeatherDto>
 }
